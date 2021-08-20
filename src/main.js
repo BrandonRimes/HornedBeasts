ff6582b7825cb28c8b294e535478d5f22131ce79
Brandon Rimes <77939170+BrandonRimes@users.noreply.github.com>
2021-08-19 23:09:40 -0400

    added filter component

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import beasts from './data/beasts.json';
 
 
 function Main(props) {
-  return beasts.map((beast, index) => <HornedBeast key={index} title={beast.title} image_url={beast.image_url} keyword={beast.keyword} description={beast.description}/>);
+  let filtered = [];
+  if (props.numHorn) {
+    filtered = beasts.filter(beast => beast.horns === parseInt(props.numHorn));
+    console.log(filtered);
+  } else {
+    filtered = beasts;
+    console.log(filtered);
+  }
+  return filtered.map((beast, index) => <HornedBeast key={index} title={beast.title} image_url={beast.image_url} keyword={beast.keyword} description={beast.description}/>);
 }
 
 export default Main;
